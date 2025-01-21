33e6042fc67e899281e9da87bfd26acb9b939b56
Scott Erickson <sderickson@gmail.com>
2025-01-21 11:22:55 -0800

    Route all /auth/* pages to auth spa in dev server

--- vue-app/vite.config.ts
+++ vue-app/vite.config.ts
@@ -3,11 +3,23 @@ import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
 import vueDevTools from "vite-plugin-vue-devtools";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vuetify(), vueDevTools()],
   server: {
     strictPort: true,
     host: true,
+
+    // For debugging, see top answer here:
+    // https://stackoverflow.com/questions/64677212/how-to-configure-proxy-in-vite
+    proxy: {
+      "^/auth/.+": {
+        rewrite: () => "http://localhost:5173/auth/",
+        target: "http://localhost:5173",
+      },
+      "^/app/.+": {
+        rewrite: () => "http://localhost:5173/app/",
+        target: "http://localhost:5173",
+      },
+    },
   },
 });
