82d9377e34f291f3a8e14373a3a83876adc63ca2
Scott Erickson <sderickson@gmail.com>
2025-01-21 13:19:22 -0800

    Set up login page validation

--- vue-app/auth/views/LoginPage.vue
+++ vue-app/auth/views/LoginPage.vue
@@ -3,71 +3,88 @@ import { ref } from "vue";
 const passwordVisible = ref(false);
 const email = ref("");
 const password = ref("");
+const valid = ref(null);
+
+const emailRules = [
+  (value: string) => !!value || "Email is required",
+  (value: string) => /.+@.+\..+/.test(value) || "Email must be valid",
+];
+
+const passwordRules = [
+  (value: string) => !!value || "Password is required",
+  (value: string) =>
+    value.length >= 8 || "Password must be at least 8 characters",
+];
 
 const login = () => {
-  console.log("login", email.value, password.value);
+  console.log("login", email.value, password.value, { valid: valid.value });
 };
 </script>
 
 <template>
   <div class="d-flex justify-center align-center flex-column fill-height">
     <v-card class="mx-auto pa-12 pb-8" elevation="8" width="448" rounded="lg">
-      <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+      <v-form v-model="valid">
+        <div class="text-subtitle-1 text-medium-emphasis">Account</div>
 
-      <v-text-field
-        v-model="email"
-        density="compact"
-        placeholder="Email address"
-        prepend-inner-icon="mdi-email-outline"
-        variant="outlined"
-      ></v-text-field>
+        <v-text-field
+          v-model="email"
+          density="compact"
+          placeholder="Email address"
+          prepend-inner-icon="mdi-email-outline"
+          variant="outlined"
+          :rules="emailRules"
+        ></v-text-field>
 
-      <div
-        class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between"
-      >
-        Password
-
-        <a
-          class="text-caption text-decoration-none text-blue"
-          href="#"
-          rel="noopener noreferrer"
-          target="_blank"
+        <div
+          class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between"
         >
-          Forgot login password?</a
-        >
-      </div>
+          Password
 
-      <v-text-field
-        v-model="password"
-        :append-inner-icon="passwordVisible ? 'mdi-eye-off' : 'mdi-eye'"
-        :type="passwordVisible ? 'text' : 'password'"
-        density="compact"
-        placeholder="Enter your password"
-        prepend-inner-icon="mdi-lock-outline"
-        variant="outlined"
-        @click:append-inner="passwordVisible = !passwordVisible"
-      ></v-text-field>
-      <v-btn
-        class="mb-8"
-        color="blue"
-        size="large"
-        variant="tonal"
-        block
-        @click="login"
-      >
-        Log In
-      </v-btn>
+          <a
+            class="text-caption text-decoration-none text-blue"
+            href="#"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Forgot login password?</a
+          >
+        </div>
 
-      <v-card-text class="text-center">
-        <a
-          class="text-blue text-decoration-none"
-          href="#"
-          rel="noopener noreferrer"
-          target="_blank"
+        <v-text-field
+          v-model="password"
+          :append-inner-icon="passwordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="passwordVisible ? 'text' : 'password'"
+          :rules="passwordRules"
+          density="compact"
+          placeholder="Enter your password"
+          prepend-inner-icon="mdi-lock-outline"
+          variant="outlined"
+          @click:append-inner="passwordVisible = !passwordVisible"
+        ></v-text-field>
+        <v-btn
+          class="my-5"
+          color="blue"
+          size="large"
+          variant="tonal"
+          block
+          :disabled="!valid"
+          @click="login"
         >
-          Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
-        </a>
-      </v-card-text>
+          Log In
+        </v-btn>
+
+        <v-card-text class="text-center">
+          <a
+            class="text-blue text-decoration-none"
+            href="#"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+          </a>
+        </v-card-text>
+      </v-form>
     </v-card>
   </div>
 </template>
