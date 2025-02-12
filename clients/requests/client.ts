d4d9ccf698d3a02c7a6e81cae2b0f133c81baef0
Scott Erickson <sderickson@gmail.com>
2025-02-12 11:42:51 -0800

    Trim options response from caddy

--- clients/requests/client.ts
+++ clients/requests/client.ts
@@ -1,6 +1,5 @@
 import createClient from "openapi-fetch";
 import type { paths } from "@saf/specs-apis/dist/openapi";
-console.log("api.docker.localhost");
 export const client = createClient<paths>({
   baseUrl: "http://api.docker.localhost/",
   credentials: "include",
