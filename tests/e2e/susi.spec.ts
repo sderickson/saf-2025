4ee616d5e6cf7a3fd5caa66d31e216f81d15b6aa
Scott Erickson <sderickson@gmail.com>
2025-02-12 16:21:22 -0800

    Add one more test

--- tests/e2e/susi.spec.ts
+++ tests/e2e/susi.spec.ts
@@ -39,3 +39,42 @@ test("registering a user", async ({ page }) => {
   await page.getByRole("button", { name: "Register" }).click();
   await expect(page.getByRole("heading", { name: "Todo List" })).toBeVisible();
 });
+
+test("full register, logout, login flow", async ({ page }) => {
+  await page.goto("http://docker.localhost/");
+  await expect(page.getByRole("link", { name: "Sign In" })).toBeVisible();
+  await page.getByRole("link", { name: "Sign In" }).click();
+  await expect(page.getByRole("link", { name: "Sign up now" })).toBeVisible();
+  await page.getByRole("link", { name: "Sign up now" }).click();
+  await expect(page.getByText("Create Account")).toBeVisible();
+
+  // signup
+  const email = getUniqueEmail();
+  await page.getByRole("textbox", { name: "Email address" }).fill(email);
+  await page.getByRole("textbox", { name: "Enter your password" }).click();
+  await page
+    .getByRole("textbox", { name: "Enter your password" })
+    .fill("asdfasdf");
+  await page.getByRole("textbox", { name: "Confirm your password" }).click();
+  await page
+    .getByRole("textbox", { name: "Confirm your password" })
+    .fill("asdfasdf");
+  await page.getByRole("button", { name: "Register" }).click();
+  await expect(page.getByRole("heading", { name: "Todo List" })).toBeVisible();
+
+  // logout
+  await page.getByRole("link", { name: "Logout" }).click();
+  await expect(page.getByRole("link", { name: "Sign In" })).toBeVisible();
+
+  // login
+  await page.getByRole("link", { name: "Sign In" }).click();
+  await page.getByRole("textbox", { name: "Email address" }).click();
+  await page.getByRole("textbox", { name: "Email address" }).fill(email);
+  await page.getByRole("textbox", { name: "Email address" }).press("Tab");
+  await page.getByRole("link", { name: "Forgot login password?" }).press("Tab");
+  await page
+    .getByRole("textbox", { name: "Enter your password" })
+    .fill("asdfasdf");
+  await page.getByRole("button", { name: "Log In" }).click();
+  await expect(page.getByRole("heading", { name: "Todo List" })).toBeVisible();
+});
