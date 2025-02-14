013519df33c07c1929fcca0e18d0e594313d9c00
Scott Erickson <sderickson@gmail.com>
2025-02-13 16:12:16 -0800

    Initial setup

---
+++ deploy/instance/setup.sh
@@ -0,0 +1,7 @@
+# This script will be the first thing that runs on a new instance
+
+# Install docker if it's not already installed
+
+# If docker is not running anything, start docker-compose base + blue
+
+# Otherwise, run the script to deploy
