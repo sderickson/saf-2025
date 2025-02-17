aebfd29807a027358b62c5832f24a1934aa52bab
Scott Erickson <sderickson@gmail.com>
2025-02-17 12:20:43 -0800

    Get production working

--- deploy/instance/build-and-push.sh
+++ deploy/instance/build-and-push.sh
@@ -2,14 +2,14 @@
 # Run from the root directory of the repository.
 
 # Build dependent images
-docker build -t saf-clients:latest -f ./clients/Dockerfile .
+docker build -t saf-clients:latest -f ./clients/Dockerfile . --platform linux/amd64
 
 # Build production images
-docker build -t ghcr.io/sderickson/saf-caddy:latest -f ./deploy/instance/Dockerfile.prod .
-docker build -t ghcr.io/sderickson/saf-services-api:latest -f ./services/api/Dockerfile .
-docker build -t ghcr.io/sderickson/saf-services-auth:latest -f ./services/auth/Dockerfile .
+docker build -t ghcr.io/sderickson/saf-caddy:latest -f ./deploy/instance/Dockerfile.prod --platform linux/amd64 .
+docker build -t ghcr.io/sderickson/saf-services-api:latest -f ./services/api/Dockerfile . --platform linux/amd64
+docker build -t ghcr.io/sderickson/saf-services-auth:latest -f ./services/auth/Dockerfile . --platform linux/amd64
 
 # Push images to GitHub's Container Registry
 docker push ghcr.io/sderickson/saf-caddy:latest
-docker push ghcr.io/sderickson/saf-specs-apis:latest
+docker push ghcr.io/sderickson/saf-services-api:latest
 docker push ghcr.io/sderickson/saf-services-auth:latest
---
+++ deploy/instance/pull.sh
@@ -0,0 +1,3 @@
+docker pull ghcr.io/sderickson/saf-caddy:latest
+docker pull ghcr.io/sderickson/saf-services-api:latest
+docker pull ghcr.io/sderickson/saf-services-auth:latest
