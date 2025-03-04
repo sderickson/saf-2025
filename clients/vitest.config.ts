7163e83cb870099b8a86b925903e5d8dde30589a
Scott Erickson <sderickson@gmail.com>
2025-03-04 10:50:16 -0800

    Clean up vitest config

--- clients/vitest.config.ts
+++ clients/vitest.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
-import { fileURLToPath } from "node:url";
+
+console.log("vitest.config.ts was run");
 
 export default defineConfig({
   plugins: [vue(), vuetify()],
   test: {
     environment: "jsdom",
     globals: true,
-    setupFiles: ["./test/setup.ts"],
     coverage: {
       provider: "v8",
       reporter: ["text", "html"],
@@ -28,18 +28,5 @@ export default defineConfig({
       inline: ["vuetify"],
     },
     mockReset: true,
-  },
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./", import.meta.url)),
-      ".css": fileURLToPath(
-        new URL("./test/__mocks__/fileMock.ts", import.meta.url),
-      ),
-    },
-  },
-  css: {
-    modules: {
-      localsConvention: "camelCase",
-    },
   },
 });
