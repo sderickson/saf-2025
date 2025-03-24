d79fcaab17c9bb6f8ced29315d27831ee5960fcb
Scott Erickson <sderickson@gmail.com>
2025-03-24 10:39:51 -0700

    Add logout page

--- lib/auth-spa/src/index.ts
+++ lib/auth-spa/src/index.ts
@@ -3,4 +3,4 @@ export * from "./types.ts";
 export { default as LoginPage } from "./components/LoginPage.vue";
 export { default as RegisterPage } from "./components/RegisterPage.vue";
 export { default as ForgotPasswordPage } from "./components/ForgotPasswordPage.vue";
-// export { default as LogoutPage } from "./views/LogoutPage.vue";
+export { default as LogoutPage } from "./components/LogoutPage.vue";
