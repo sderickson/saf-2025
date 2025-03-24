22923e515f108c859b6e53cd31f460a1a6413e44
Scott Erickson <sderickson@gmail.com>
2025-03-24 10:17:38 -0700

    Export login, register pages

--- lib/auth-spa/src/index.ts
+++ lib/auth-spa/src/index.ts
@@ -1,2 +1,6 @@
 export * from "./requests/auth.ts";
 export * from "./types.ts";
+export { default as LoginPage } from "./components/LoginPage.vue";
+export { default as RegisterPage } from "./components/RegisterPage.vue";
+// export { default as ForgotPasswordPage } from "./views/ForgotPasswordPage.vue";
+// export { default as LogoutPage } from "./views/LogoutPage.vue";
