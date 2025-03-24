841288f00573ac4fde9f7762d7e5d37a739e5135
Scott Erickson <sderickson@gmail.com>
2025-03-24 08:18:27 -0700

    get tests working

--- lib/auth-spa/src/types.ts
+++ lib/auth-spa/src/types.ts
@@ -1,4 +1,5 @@
-import type { RequestSchema } from "@saf-2025/ts-openapi";
-
-export type LoginRequest = RequestSchema<"loginUser">;
-export type RegisterRequest = RequestSchema<"registerUser">;
+export type {
+  LoginRequest,
+  RegisterRequest,
+  UserResponse,
+} from "@saf/auth-spec";
