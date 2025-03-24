53caca7905dd80ab78dd73ae2dcf6d3e2c804de5
Scott Erickson <sderickson@gmail.com>
2025-03-24 10:32:33 -0700

    Move over forgot password page

--- lib/auth-spa/src/index.ts
+++ lib/auth-spa/src/index.ts
@@ -2,5 +2,5 @@ export * from "./requests/auth.ts";
 export * from "./types.ts";
 export { default as LoginPage } from "./components/LoginPage.vue";
 export { default as RegisterPage } from "./components/RegisterPage.vue";
-// export { default as ForgotPasswordPage } from "./views/ForgotPasswordPage.vue";
+export { default as ForgotPasswordPage } from "./components/ForgotPasswordPage.vue";
 // export { default as LogoutPage } from "./views/LogoutPage.vue";
