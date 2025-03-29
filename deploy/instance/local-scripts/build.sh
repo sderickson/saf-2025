3cc85e3ffa054240558699ac1ef927d27018e7e3
Scott Erickson <sderickson@gmail.com>
2025-03-29 14:30:47 -0700

    Move clients into a subfolder - spas.

--- deploy/instance/local-scripts/build.sh
+++ deploy/instance/local-scripts/build.sh
@@ -2,7 +2,7 @@
 # Build dependent images
 set -e
 
-docker build -t saf-2025-clients:latest -f ./clients/Dockerfile . --platform linux/amd64
+docker build -t saf-2025-spa-clients:latest -f ./clients/spas/Dockerfile . --platform linux/amd64
 
 # Build production images
 docker build -t ghcr.io/sderickson/saf-2025-caddy:latest -f ./deploy/instance/Dockerfile.prod --platform linux/amd64 .
