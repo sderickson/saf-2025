8659c6c77eeeb0835e7771d1e4b39b33988082cf
Scott Erickson <sderickson@gmail.com>
2025-04-04 10:59:14 -0700

    Adopt added auth-service security (#27)
    Partner PR to https://github.com/sderickson/saflib/pull/8
    
    * Adopt `createSafClient`
    * Get playwright tests to work via disabling rate limiting
    * Adopt new ExtractResponseSchema, ExtractRequestSchema
    
    And by using the code from the saflib PR, I'm getting a much-upgraded
    `auth-service` implementation.
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **Refactor**
    	- Streamlined the API client setup to simplify interactions.
    - Updated the task endpoint validation logic with a standardized request
    and response structure.
    
    - **Chores**
    - Upgraded a foundational dependency to integrate the latest underlying
    improvements.
    - Added a new environment variable `DISABLE_RATE_LIMITING` to
    configuration files.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- clients/spas/requests/client.ts
+++ clients/spas/requests/client.ts
@@ -1,17 +1,4 @@
-import createClient from "openapi-fetch";
-import type { types } from "@saf-2025/specs-apis";
-export const client = createClient<types.paths>({
-  fetch: (request: Request) => {
-    // this is little noop wrapper is required for msw to work
-    const csrfToken = document.cookie
-      .split("; ")
-      .find((row) => row.startsWith("_csrf_token="))
-      ?.split("=")[1];
-    if (csrfToken) {
-      request.headers.set("X-CSRF-Token", csrfToken);
-    }
-    return fetch(request);
-  },
-  baseUrl: `${document.location.protocol}//api.${document.location.host}`,
-  credentials: "include",
-});
+import type { paths } from "@saf-2025/specs-apis";
+import { createSafClient } from "@saflib/vue-spa";
+
+export const client = createSafClient<paths>("api");
--- specs/apis/index.ts
+++ specs/apis/index.ts
@@ -1,8 +1,14 @@
-import type { RequestSchema, ResponseSchema } from "./helpers.ts";
+// types
+import type { operations } from "./dist/openapi.d.ts";
+export type { paths, components, operations } from "./dist/openapi.d.ts";
+import type {
+  ExtractResponseSchema,
+  ExtractRequestSchema,
+} from "@saflib/openapi-specs";
+export type ApiRequestSchema = ExtractRequestSchema<operations>;
+export type ApiResponseSchema = ExtractResponseSchema<operations>;
+
+// json
 import * as json from "./dist/openapi.json" with { type: "json" };
 import type { OpenAPIV3 } from "express-openapi-validator/dist/framework/types.ts";
-import type * as types from "./dist/openapi.d.ts";
-
 export const jsonSpec = (json as any).default as OpenAPIV3.DocumentV3;
-
-export type { RequestSchema, ResponseSchema, types };
