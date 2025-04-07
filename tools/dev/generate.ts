f92cd74728654b74d76602ee78aef3c9a2d52491
Scott Erickson <sderickson@gmail.com>
2025-04-07 13:04:34 -0700

    Refactor dev tools into saflib, get build to work

--- tools/dev/generate.ts
+++
@@ -1,7 +0,0 @@
-import {
-  buildMonorepoContext,
-  generateDockerfiles,
-} from "./src/generate-dockerfile.ts";
-
-const monorepoContext = buildMonorepoContext("../../");
-generateDockerfiles(monorepoContext);
