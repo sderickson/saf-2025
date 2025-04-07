1d5b46208f17af31d5d5e4fe4b233daf19f8af94
Scott Erickson <sderickson@gmail.com>
2025-04-07 08:48:05 -0700

    got nodemailer working

--- services/api/app.ts
+++ services/api/app.ts
@@ -9,6 +9,42 @@ import express from "express";
 import { preMiddleware, errorHandlers } from "./middleware.ts";
 import todosRouter from "./routes/todos.ts";
 
+import nodemailer from "nodemailer";
+
+const config = {
+  host: "smtp-relay.gmail.com",
+  secure: true,
+  logger: true,
+  debug: true,
+  connectionTimeout: 5000,
+};
+console.log("smtp transporter config", config);
+const transporter = nodemailer.createTransport(config);
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP server connection error:", error);
+  } else {
+    console.log("SMTP server connection successful", success);
+  }
+});
+
+const testEmail = async () => {
+  try {
+    const info = await transporter.sendMail({
+      from: "",
+      to: "",
+      subject: "Test Email",
+      text: "Hello, Scott!",
+    });
+    console.log("Message sent: %s", info.messageId);
+    console.log("info", info);
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+};
+
+testEmail();
+
 const app = express();
 app.set("trust proxy", true);
 
