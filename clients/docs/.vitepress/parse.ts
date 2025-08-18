71f2d6dad3de2ca2908ef63aa86e4b97d4204857
Scott Erickson <sderickson@gmail.com>
2025-08-18 09:59:53 -0700

    updates

--- clients/docs/.vitepress/parse.ts
+++ clients/docs/.vitepress/parse.ts
@@ -11,29 +11,7 @@ export interface packageInfo {
   docs: document[];
 }
 
-function findMarkdownFiles(dir: string): string[] {
-  const files: string[] = [];
-  const items = readdirSync(dir);
-
-  for (const item of items) {
-    const fullPath = join(dir, item);
-    const stat = statSync(fullPath);
-
-    if (stat.isDirectory()) {
-      files.push(...findMarkdownFiles(fullPath));
-    } else if (item.endsWith(".md")) {
-      files.push(fullPath);
-    }
-  }
-
-  return files;
-}
-
-interface accumulatedDocs {
-  [key: string]: packageInfo;
-}
-
-const getDocsByPackage2 = (rootPath: string) => {
+const getDocsByPackage = (rootPath: string) => {
   const monorepoContext = buildMonorepoContext(rootPath);
   const packages = Array.from(monorepoContext.packages)
     .filter((packageName) => {
@@ -65,55 +43,20 @@ const getDocsByPackage2 = (rootPath: string) => {
           };
         }),
       };
+
+      const refPath = join(docsDir, "ref", "index.md");
+      if (existsSync(refPath)) {
+        const relativePath = refPath.replace(rootPath, "");
+        info.docs.push({
+          text: "Reference",
+          link: relativePath,
+        });
+      }
       return info;
     })
     .filter((p) => p !== false);
   console.log("packages", packages);
   return packages;
-};
-
-const getDocsByPackage = (rootPath: string) => {
-  return getDocsByPackage2(rootPath);
-
-  const markdownFiles = findMarkdownFiles(rootPath);
-
-  const docsByPackage: accumulatedDocs = markdownFiles.reduce(
-    (acc: accumulatedDocs, file: string) => {
-      // console.log("file", file);
-      const relativePath = file.replace(rootPath, "");
-      const numberOfDirectories = relativePath.split("/").length;
-      if (numberOfDirectories < 3) {
-        // e.g. "/README.md"
-        return acc;
-      }
-
-      const packageName = relativePath.split("/").slice(1, 2).join("/");
-      if (!acc[packageName]) {
-        const packageJsonPath = join(rootPath, packageName, "package.json");
-        if (!existsSync(packageJsonPath)) {
-          return acc;
-        }
-        acc[packageName] = {
-          name: packageName,
-          docs: [],
-        };
-      }
-
-      if (relativePath.split("/").includes("docs")) {
-        const firstLine = readFileSync(file, "utf8").split("\n")[0];
-        const text = firstLine.replace("#", "").trim();
-        const link = relativePath;
-        acc[packageName].docs.push({
-          text,
-          link,
-        });
-      }
-      return acc;
-    },
-    {},
-  );
-  // console.log(docsByPackage);
-  return docsByPackage;
 };
 
 export { getDocsByPackage };
